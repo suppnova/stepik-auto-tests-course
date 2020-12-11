3b1edbd938e6546a9f65ea893c2dbdb3d24eea0c
Natalia Odintsova <nata-odintsova@mail.ru>
2020-12-11 14:23:27 +0300

    implemented autotest for registration using unique selectors

---
+++ 1.6_unique.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Ваш код, который заполняет обязательные поля
+    input1 = browser.find_element_by_css_selector(".first[required]")
+    input1.send_keys("Nata")
+    input2 = browser.find_element_by_css_selector(".second[required]")
+    input2.send_keys("S")
+    input3 = browser.find_element_by_css_selector(".third[required]")
+    input3.send_keys("m@mail.ru")
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+except NoSuchElementException:
+    print("NoSuchElementException")
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
